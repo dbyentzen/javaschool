3a069d1f78ef0013edd92ee1f8aaa0de50c1695a
dbyentzen <dbyentzen@gmail.com>
2016-03-14 13:00:24 -0500

    commit for a simple calendar

---
+++ SimpleCalendar2016.java
@@ -0,0 +1,63 @@
+/*
+  A simple 2016 Calendar program. 
+  This calendar does not include
+  the days of the week.
+*/
+class SimpleCalendar2016 {
+    public static void main(String args[]) {
+      System.out.println("Calendar for 2016");
+      System.out.println();
+      
+      for(int j = 1; j < 32; j++) {
+      System.out.println("January " + j + " 2016");
+      }
+      
+      for(int f = 1; f < 30; f++) {
+      System.out.println("February " + f + " 2016");
+      }
+      
+      for(int m = 1; m < 32; m++) {
+      System.out.println("March " + m + " 2016");
+      }
+      
+      for(int a = 1; a < 31; a++) {
+      System.out.println("April " + a + " 2016");
+      }
+      
+      for(int y = 1; y < 32; y++) {
+      System.out.println("May " + y + " 2016");
+      }
+      
+      for(int u = 1; u < 31; u++) {
+      System.out.println("June " + u + " 2016");
+      }
+      
+      for(int l = 1; l < 32; l++) {
+      System.out.println("July " + l + " 2016");
+      }
+      
+      for(int g = 1; g < 32; g++) {
+      System.out.println("August " + g + " 2016");
+      }
+      
+      for(int s = 1; s < 31; s++) {
+      System.out.println("September " + s + " 2016");
+      }
+      
+      for(int o = 1; o < 32; o++) {
+      System.out.println("October " + o + " 2016");
+      }
+      
+      for(int n = 1; n < 31; n++) {
+      System.out.println("November " + n + " 2016");
+      } 
+      
+      for(int d = 1; d < 32; d++) {
+      System.out.println("December " + d + " 2016");
+      }
+    }
+ }   
+     
+      
+      
+      
